3ebaa006a28465b1f14fc5905360def6afb5ba25
Ahmed Elkenawy <ahmed.elkenawy95@gmail.com>
2019-07-26 16:59:31 +0200

    edit header

--- client/phoneBook/src/app/core/interceptor/http.headerinterceptor.ts
+++ client/phoneBook/src/app/core/interceptor/http.headerinterceptor.ts
@@ -12,15 +12,14 @@ import { Observable } from "rxjs";
   providedIn: "root"
 })
 /**
- * inercept the request and add header
- * add the token as a header
+ * inercept the request and add header and Token
+ *
  */
 export class HeaderInterceptor implements HttpInterceptor {
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem("token");
     const httpHeader = {
       "Content-Type": "application/json"
     };
